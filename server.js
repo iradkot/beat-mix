537ce6642af6a44513cce5802b74b6c88b0e9439
unknown <mosh7890@gmail.com>
2017-08-21 15:40:50 +0300

    fixes

--- server.js
+++ server.js
@@ -1,17 +1,57 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const path = require("path");
+const nodemailer = require("nodemailer");
 
 const app = express();
 
-// tell the app to look for static files in these directories
+//* tell the app to look for static files in these directories
 app.use(express.static("./server/static/"));
 app.use(express.static("./client/dist/"));
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
 
-//Handle browser refresh by redirecting to index html
+//* nodemailer Email
+var transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true, //! secure:true for port 465, secure:false for port 587
+  auth: {
+    user: "elevationmosh@gmail.com",
+    pass: "Elevation358"
+  }
+});
+
+//* email route
+app.post("/contactus/email", function(req, res) {
+  let data = req.body;
+  let mailOptions1 = {
+    from: "elevationmosh@gmail.com",
+    to: "mosh7890@gmail.com",
+    subject: `${data.name} - ${data.email} - ${data.number}`,
+    text: data.content
+  };
+
+  transporter.sendMail(mailOptions1, function(error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
+    }
+  });
+  res.send("Sent");
+});
+
+//* Handle browser refresh by redirecting to index html
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "./server/static/index.html"));
 });
-// start the server
+
+//* Start the server
 app.listen(3000, () => {
   console.log(
     "Server is running on http://localhost:3000 or http://127.0.0.1:3000"
