802f5f30269d700925f70f97818aa32b76165090
mosh7890 <mosh7890@gmail.com>
2017-08-22 11:55:17 +0300

    components

---
+++ client/src/BeatMix/FbPlugins.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+class FbPlugins extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="fb-divs">
+        <div
+          className="fb-comments"
+          data-href="https://www.facebook.com/elevationmosh"
+          data-numposts="5"
+        />
+        <div
+          className="fb-like"
+          data-href="https://www.facebook.com/elevationmosh/"
+          data-layout="button_count"
+          data-action="like"
+          data-size="large"
+          data-show-faces="true"
+          data-share="false"
+        />
+        <div
+          className="fb-follow"
+          data-href="https://www.facebook.com/elevationmosh/"
+          data-layout="button"
+          data-size="large"
+          data-show-faces="true"
+        />
+      </div>
+    );
+  }
+}
+export default FbPlugins;
