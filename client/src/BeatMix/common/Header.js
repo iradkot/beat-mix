7a0908bd49ed9badf62b8b44f8353240d9f5c9a2
mosh7890 <mosh7890@gmail.com>
2017-08-31 12:35:47 +0300

    navbar

--- client/src/BeatMix/common/Header.js
+++ client/src/BeatMix/common/Header.js
@@ -1,68 +1,50 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Navbar, Nav, NavItem, MenuItem, NavDropdown } from "react-bootstrap";
 
 class Header extends React.Component {
   render() {
     return (
-      <nav className="navbar navbar-inverse navbar-fixed-top">
-        <div className="container-fluid">
-          <div className="navbar-header">
-            <button
-              type="button"
-              className="navbar-toggle"
-              data-toggle="collapse"
-              data-target="#myNavbar"
-            >
-              <span className="icon-bar" />
-              <span className="icon-bar" />
-              <span className="icon-bar" />
-            </button>
+      <Navbar fluid collapseOnSelect inverse fixedTop>
+        <Navbar.Header>
+          <Navbar.Brand>
             <Link className="navbar-brand navbar-right" to="/">
               <img
                 id="navbarBrand"
                 src="http://beatmix.mitchatnim.co.il/img/0239/799.png"
               />
             </Link>
-          </div>
-          <div className="navbar-collapse collapse" id="myNavbar">
-            <ul className="nav navbar-nav navbar-right">
-              <li>
-                <a target="_blank" href="https://www.facebook.com/beatmixdjs">
-                  פייסבוק
-                </a>
-              </li>
-              <li>
-                <Link to="/ContactUs">צרו-קשר</Link>
-              </li>
-              <li className="dropdown">
-                <a className="dropdown-toggle" data-toggle="dropdown" href="#">
-                  גלריות <span className="caret" />
-                </a>
-                <ul className="dropdown-menu">
-                  <li>
-                    <Link to="/TamAmit">תם ועמית</Link>
-                  </li>
-                  <li>
-                    <Link to="/Artists">נוי</Link>
-                  </li>
-                  <li>
-                    <Link to="/Events">אירועים</Link>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <Link to="/WhyBeatMix">למה דאבל דיג'ייז</Link>
-              </li>
-              <li>
-                <Link to="/AboutUs">נבחרת הדיג'יים</Link>
-              </li>
-              <li>
-                <Link to="/">דף הבית</Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav pullRight>
+            <NavItem eventKey={1} href="https://www.facebook.com/beatmixdjs">
+              פייסבוק
+            </NavItem>
+            <NavItem eventKey={2}>
+              <Link to="/ContactUs">צרו-קשר</Link>
+            </NavItem>
+            <NavDropdown eventKey={3} title="גלריות" id="basic-nav-dropdown">
+              <MenuItem eventKey={3.1}>
+                <Link to="/Artists">אמנים</Link>
+              </MenuItem>
+              <MenuItem eventKey={3.2}>
+                <Link to="/Events">אירועים</Link>
+              </MenuItem>
+            </NavDropdown>
+            <NavItem eventKey={4}>
+              <Link to="/WhyBeatMix">למה דאבל דיג'ייז</Link>
+            </NavItem>
+            <NavItem eventKey={5}>
+              <Link to="/AboutUs">נבחרת הדיג'יים</Link>
+            </NavItem>
+            <NavItem eventKey={6}>
+              <Link to="/">דף הבית</Link>
+            </NavItem>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     );
   }
 }
